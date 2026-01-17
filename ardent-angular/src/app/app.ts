59a6abad0a8cf55697681dd9d96f206486e89414
Shrey <mishra.shrey001@gmail.com>
2026-01-17 23:32:32 +0530

    Added navbar , carousel and footer components

--- ardent-angular/src/app/app.ts
+++ ardent-angular/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./layout/navbar/navbar";
+import { Carousel } from "./components/carousel/carousel";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Carousel, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
