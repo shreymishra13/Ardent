df6cd919ad14849fbb0e309108869936f969fe72
Shrey <mishra.shrey001@gmail.com>
2026-01-28 13:08:57 +0530

    Added the api to fetch the book dynamically from DB

--- ardent-angular/src/app/pages/books/books.ts
+++ ardent-angular/src/app/pages/books/books.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-books',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './books.html',
   styleUrl: './books.css',
 })
-export class Books {
+export class Books implements OnInit{
+
+  private http = inject(HttpClient);
+
+  books : any[] = [];
+  ngOnInit(): void {
+
+    this.getAllBooks();
+
+    
+  }
+
+  getAllBooks(){
+    this.http.get("http://localhost:8080/books").subscribe((res : any)=>{
+      this.books = res.data;
+      console.log(this.books);
+    })
+  }
+  
 
 }
